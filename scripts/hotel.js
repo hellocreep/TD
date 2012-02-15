6ed44a989dd457e5ea4fbad133b6cb0d09b8e8c2
chenzhao <remaintears@gmail.com>
2012-02-15 09:26:56 +0800

    review

--- scripts/hotel.js
+++ scripts/hotel.js
@@ -2,24 +2,33 @@
 $( function(){
 			
 var hotel = new Array(1);
+var hotelhistory='';
 	$('.hotelreviewd' ).each( function(){
 		$(this).click(function( e ){
 			e.preventDefault();
 			var hname=$(this).text();
-
-			for( var i = 0; i<hotel.length; i++)
+			var hid=$(this).attr("val");
+			if (hotelhistory.indexOf(hid+','+hname)<0)
 			{
-				if( hotel.indexOf(hname)==-1){ // hotel²»´æÔÚ
-					hotel.push(hname);
-					}
+				hotelhistory +=hid+','+hname+',';
 			}
+			alert(hotelhistory);
+			if( hotel.indexOf(hname)==-1)
+			{
+				
+			}
+			
 			$('.viewedhotel').remove();
-			$.cookie('hotel',hotel);
+			$.cookie('hotel',hotel[hid]);
 			var arr=$.cookie('hotel').split(',');
 			var strhtml='';
 			for(var i=1; i<arr.length;i++)
 			{
+				if(arr[i]!='')
+				{
+					
 				strhtml+='<div class="viewedhotel"><div class="viewhotelList"><div class="viewhotelPic"><img src="http://images.chinahighlights.com/hotel/Shanghai/the-bund-hotel/views-s.jpg" width="50" height="50" class="hotelSmal" original="http://images.chinahighlights.com/hotel/Shanghai/the-bund-hotel/views-s.jpg"></div><div class="viewhotelInfo"><h5><a href="/hotel/shanghai-hotels/the-bund-hotel.htm">'+arr[i]+'</a><img alt="4 Star" src="/images/reviewd/fourstar-s.png" original="/images/reviewd/fourstar-s.png"></h5></div><a class="closeWindow" val="5885" onclick="closeThis(this);" href="javascript:void(0)"> </a><div class="clear"></div></div></div>';
+				}
 			}
 			$('#hotelviwed').after(strhtml);
 			
@@ -30,10 +39,10 @@ var hotel = new Array(1);
 	{
 		var hotelid = $(obj).attr("val");
 		var Ext = "$"+ hotelid +"$";
-		var NewCK = $.cookie("hotelHistory").replace(Ext,"");
-		$.cookie("hotelHistory",NewCK);
-		$(Obj).parents(".viewhotelList").slideUp(200);
-		setTimeout(function(){$(Obj).parents(".viewhotelList").remove()}, 200);
+		var NewCK = $.cookie("hotel").replace(Ext,"");
+		$.cookie("hotel",NewCK);
+		$(obj).parents(".viewhotelList").slideUp(200);
+		setTimeout(function(){$(obj).parents(".viewhotelList").remove()}, 200);
 		return false; 
 	}
 
