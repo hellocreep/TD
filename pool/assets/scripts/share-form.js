f73494237304d7d71647ad89e43c9478c228d3a5
chenzhao <remaintears@gmail.com>
2012-03-14 10:08:01 +0800

    share-form

--- pool/assets/scripts/share-form.js
+++ pool/assets/scripts/share-form.js
@@ -1,31 +1,49 @@
-/**
- * @author chenzhao
- */
-(function(){
+;(function(){
+	var right_type=new Array(".gif",".jpg",".jpeg",".png",".bmp");
+	 function checkImgType(fileURL)
+     {
+         var right_typeLen=right_type.length;
+         var imgUrl=fileURL.toLowerCase();
+         var postfixLen=imgUrl.length;
+         var len4=imgUrl.substring(postfixLen-4,postfixLen);
+         var len5=imgUrl.substring(postfixLen-5,postfixLen);
+         for (i=0;i<right_typeLen;i++)
+         {
+             if((len4==right_type[i])||(len5==right_type[i]))
+             {
+                 return true;
+             }
+         }
+     }
 $(function(){
-	$('#shareForm').submit(function(){
-		 $("#shareForm").validate();
-		 
-		 if($("#shareForm").valid())
-		 {
-		 	return true;
-		 }
-		 else
-		 {
-		 	return false;
-		 }
-	});
-	
-	$( '.send-more' ).click(function(){
-		if ($( '.attach' ).length > 6)
+		$('#shareForm').validate();
+
+	$('.Sendmore').click(function(){
+		if($('.uploade').length>5)
 		{
 			return false;
 		}
-		else{
-			//$( '.attach' ).clone().val( ' ' ).show().insertBefore( $(this) );
-			$( this ).parent().parent()
-			.before( '<tr class="attach"><td colspan="2"><input type="file" name="pic[]" class="upfile"></td></tr>' );
+		else
+		{
+			$('#new_up').append('<input type="file" name="pic[]" class="uploade"></br>');
 		}
-	})
-})
+	});
+
+	$('.uploade').each(function(){
+		$(this).change(function(){
+
+			if(!checkImgType($(this).val()))
+         	{
+             alert("The file type is not correct ! Must be ('.gif','.jpg','.jpeg','.png','.bmp') ");
+             $(this).css('background','#FF0000');
+             return;
+        	}
+        	else
+        	{
+        		$(this).css('background','');
+        	}
+		});
+	});
+	
+});
 })(jQuery)
