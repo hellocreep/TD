99ead573fd7fa6939d6cc7ec1996ac1e9d09b5b8
liwen <hellocreep@gmail.com>
2012-03-28 14:29:56 +0800

    ...

--- pool/assets/scripts/group-tour.js
+++ pool/assets/scripts/group-tour.js
@@ -1,14 +1,39 @@
 (function(){
-function get_schedule(){
+function get_schedule(id,y,m){
 	$.ajax({
-		url: '//',
+		url: 'http://www.tibetdiscovery.com/assets/snippets/grouptour/group-tour.php',
+		type:'GET',
+		dataType:'html',
 		data: {
-			tour_id: '',
-			month: '',
-			year: '',
+			tour_id: id,
+			month: m,
+			year: y
 		},
 		success: function( result ){
-			
+			$('.schedule-wrap').html(result);
+			$( '.schedule-arrow' ).each(function(){
+				$( this ).click(function(){
+				var tourid=$(this).parent.attr('name');
+				var year=$(this).siblings('.schedule-year').text();
+				var month;
+				if( $(this).hasClass('arrow-prev') ){
+					month = $( this ).siblings( '.schedule-month' ).attr( 'name' )*1-1;
+				}else{
+					month = $( this ).siblings( '.schedule-month' ).attr( 'name' )*1+1;
+				}
+				if(month==0)
+				{
+					month=12;
+					year=year-1;
+				}
+				else if(month==13)
+				{
+					month=1;
+					year=year+1;
+				}
+				get_schedule(tourid,year,month);	
+			});
+		});
 		}
 	});
 }
@@ -20,17 +45,38 @@ $(function(){
 	
 	$( '.schedule-arrow' ).each(function(){
 		$( this ).click(function(){
-			
+			var tourid=$(this).parent.attr('name');
+			var year=$(this).siblings('.schedule-year').text();
+			var month;
+			if( $(this).hasClass('arrow-prev') ){
+				month = $( this ).siblings( '.schedule-month' ).attr( 'name' )*1-1;
+			}else{
+				month = $( this ).siblings( '.schedule-month' ).attr( 'name' )*1+1;
+			}
+			if(month==0)
+			{
+				month=12;
+				year=year-1;
+			}
+			else if(month==13)
+			{
+				month=1;
+				year=year+1;
+			}
+			alert(tourid);
+			alert(year);
+			alert(month);
+			//get_schedule(tourid,year,month);	
 		});
 	});
 	
-	$(' .addwish').fancybox({
+	$( '.addwish' ).fancybox({
 		'width' : '50%',
 		'scrolling': 'auto',
 		'height': '75%',
 		'transitionIn' : 'none',
 		'transitionOut' : 'none',
-		'type' : 'iframe',
+		'type' : 'iframe'
 		//'onClosed':function(){window.location.href=window.location.href;}
 	}); 
 });
