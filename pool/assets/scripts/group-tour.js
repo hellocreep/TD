67723b79bc057f1d3815b7e5dd945c41b7aae56a
chenzhao <remaintears@gmail.com>
2012-03-28 14:27:51 +0800

    group-tour

--- pool/assets/scripts/group-tour.js
+++ pool/assets/scripts/group-tour.js
@@ -1,14 +1,44 @@
 (function(){
-function get_schedule(){
+function get_schedule(id,y,m){
 	$.ajax({
-		url: '//',
+		url: 'http://www.tibetdiscovery.com/assets/snippets/grouptour/group-tour.php',
+		type:'POST',
+		dataType:'html',
 		data: {
-			tour_id: '',
-			month: '',
-			year: '',
+			tour_id: id,
+			month: m,
+			year: y
 		},
 		success: function( result ){
-			
+			$('.schedule-wrap').html(result);
+			$( '.schedule-arrow' ).each(function(){
+				$( this ).click(function(){
+					var tourid=$(this).parent().attr('name');
+					var year=$(this).siblings('.schedule-year').text();
+					var month;
+					var date=new Date();
+					if( $(this).hasClass('arrow-prev') ){
+						month = $( this ).siblings( '.schedule-month' ).attr( 'name' )*1-1;
+					}else{
+						month = $( this ).siblings( '.schedule-month' ).attr( 'name' )*1+1;
+					}
+					var monthnow=date.getMonth();
+					if(month==0)
+					{
+						month=12;
+						year=year-1;
+					}
+					else if(month==13)
+					{
+						month=1;
+						year=year+1;
+					}
+					if(month>=monthnow)
+					{
+						get_schedule(tourid,year,month);	
+					}
+				});
+			});
 		}
 	});
 }
@@ -20,17 +50,40 @@ $(function(){
 	
 	$( '.schedule-arrow' ).each(function(){
 		$( this ).click(function(){
-			
+			var tourid=$(this).parent().attr('name');
+			var year=$(this).siblings('.schedule-year').text();
+			var month;
+			var date=new Date();
+			if( $(this).hasClass('arrow-prev') ){
+				month = $( this ).siblings( '.schedule-month' ).attr( 'name' )*1-1;
+			}else{
+				month = $( this ).siblings( '.schedule-month' ).attr( 'name' )*1+1;
+			}
+			var monthnow=date.getMonth();
+			if(month==0)
+			{
+				month=12;
+				year=year-1;
+			}
+			else if(month==13)
+			{
+				month=1;
+				year=year+1;
+			}
+			if(month>=monthnow)
+			{
+				get_schedule(tourid,year,month);	
+			}
 		});
 	});
 	
-	$(' .addwish').fancybox({
+	$( '.addwish' ).fancybox({
 		'width' : '50%',
 		'scrolling': 'auto',
 		'height': '75%',
 		'transitionIn' : 'none',
 		'transitionOut' : 'none',
-		'type' : 'iframe',
+		'type' : 'iframe'
 		//'onClosed':function(){window.location.href=window.location.href;}
 	}); 
 });
