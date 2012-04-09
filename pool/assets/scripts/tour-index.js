eeecefb56a7ab331b27f3a10949a9475342a76a9
liwen <hellocreep@gmail.com>
2012-04-09 15:15:02 +0800

    tour-index css

---
+++ pool/assets/scripts/tour-index.js
@@ -0,0 +1,27 @@
+(function(){
+	
+	var _timer_;
+	function mainautoshow( imgs, intime, outtime, delay ) {
+		$( imgs+" img:not(:last)" ).hide();	
+		_timer_ = setInterval( function() {
+			var activemainAd = $( imgs+" img:visible" ),
+			next = activemainAd.next();
+			if ( !next.length ) {
+				next = $( imgs+" img:first" );
+			}
+			next.show().css( 'z-index', 0 );
+			activemainAd.fadeOut( outtime,function(){
+				next.css( 'z-index',1 );
+			});
+		}, delay );
+	} 
+$(function(){
+	
+	mainautoshow( '.banner',1000,2000,5000);
+	
+	$( '.tour-rank' ).each(function( index ){
+		$( this ).text( index+1 );
+	});
+	
+})
+})(jQuery);
