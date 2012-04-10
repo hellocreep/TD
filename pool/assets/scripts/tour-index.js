ea45294d04334d10fa8e70c13a62340146814c78
liwen <hellocreep@gmail.com>
2012-04-10 16:45:42 +0800

    ..

--- pool/assets/scripts/tour-index.js
+++ pool/assets/scripts/tour-index.js
@@ -14,10 +14,30 @@
 				next.css( 'z-index',1 );
 			});
 		}, delay );
-	} 
+	}
+	
+	function slideshow(){
+		$( '.map-list li' ).each(function( index ){
+			$( this ).hover(function(){
+				$( this ).children( '.crystal-bubble' ).fadeIn();
+				$( this ).addClass( 'active' ).siblings().removeClass( 'active' );
+				$( '.banner > img' ).eq( index ).fadeIn().siblings( 'img' ).hide();
+			},function(){
+				$( this ).children( '.crystal-bubble' ).fadeOut();
+			});
+		});
+	}
 $(function(){
 	
-	mainautoshow( '.banner',1000,2000,5000);
+	if( $('.allpage-wrap').attr('name')=='china'){
+		$( '.expat' ).hide();
+		$( '.banner > img:not(:first)' ).hide();
+		slideshow();
+		$( '.allpage-wrap' ).removeClass( 'BL-color' ).addClass( 'china-color' );
+		$( '.tour-block' ).css( 'background','url(/imagesnew/tour-index/bg-tour.png) repeat-x 0 0 #ffffff');
+	}else{
+		mainautoshow( '.banner',1000,2000,5000);
+	}
 	
 	$( '.tour-rank' ).each(function( index ){
 		$( this ).text( index+1 );
@@ -25,5 +45,11 @@ $(function(){
 	$( '.tour-price' ).eq( 3 ).hide();
 	
 	$( '.fancybox' ).fancybox();
+	$( '.fancybox' ).hover(function(){
+			$(this).append('<span class="zoom"></span>');
+		},function(){
+			$(this).children('.zoom').remove();
+	}); 
+	
 })
 })(jQuery);
