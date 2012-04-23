9a262d8e9c79d19ddbcf6a5832c5a69ec67528f7
welkang <welkang@qq.com>
2012-04-23 16:26:07 +0800

    yes

--- pool/assets/scripts/index.js
+++ pool/assets/scripts/index.js
@@ -1,26 +1,33 @@
-(function(){
-	function AutoScroll(){
-		$('.Recent-Inquiry').find("ul:first").animate({
-		marginTop:"-25px"
-		},500,function(){
-			$(this).css({marginTop:"0px"}).find("li:first").appendTo(this);
-		});
-	} 
-$(function(){
-	//首页banner轮播
-	$(document).ready(function() {
+(function() {
+
+	/*function AutoScroll(){
+	 $('.Recent-Inquiry').find("ul:first").animate({
+	 marginTop:"-25px"
+	 },500,function(){
+	 $(this).css({marginTop:"0px"}).find("li:first").appendTo(this);
+	 });
+	 }
+	 setInterval(AutoScroll, 5000);
+	 * */
+
+	$(function() {
+		//首页banner轮播
+
 		$(".banner ul li:gt(0)").hide();
 		setInterval(function() {
 			$('.banner ul li:first').fadeOut(3000).next().fadeIn(3000).end().appendTo('.banner ul');
 		}, 5000);
+		
+		//首页recent-inquiry效果
+		setInterval(function() {
+			$('.Recent-Inquiry ul li:first').slideUp(1500, function() {;
+				$(this).appendTo('.Recent-Inquiry ul').show();
+			})
+		}, 3000);
+		
+		//Expat tour toggle caption
+		$('.expat-list a').hover(function() {
+			$(this).children('.caption').children('p').slideToggle(300);
+		});
 	})
-
-	/******recent inquery 文字滚动**************/
-	setInterval(AutoScroll,5000);
-
-	//Expat tour toggle caption
-	$( '.expat-list a' ).hover(function(){
-    		$( this ).children( '.caption' ).children( 'p' ).slideToggle(300);
-	});
-})
 })(jQuery);
